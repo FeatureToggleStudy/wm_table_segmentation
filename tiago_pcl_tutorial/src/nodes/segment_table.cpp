94022c90728a5f53b77d88b0e0a52debbf3c5190
philippe <philippe117@live.fr>
2019-06-01 17:29:28 -0400

    corrige correctement l'attente du tf

--- tiago_pcl_tutorial/src/nodes/segment_table.cpp
+++ tiago_pcl_tutorial/src/nodes/segment_table.cpp
@@ -189,13 +189,13 @@ namespace pal {
     if ( !_processingFrame.empty() )
     {
       cloudInProcFrame.reset(new sensor_msgs::PointCloud2);
-      ROS_INFO_STREAM("Transforming point cloud from frame " << cloud->header.frame_id << " to frame " << _processingFrame);
+      ROS_INFO_STREAM("segtable Transforming point cloud from frame " << cloud->header.frame_id << " to frame " << _processingFrame);
         std::string errMsg;
         _tfListener.waitForTransform(_processingFrame,
                                      cloud->header.frame_id,
-                                     ros::Time(0),
+                                     cloud->header.stamp,
                                      ros::Duration(4),
-                                     ros::Duration(0.01),
+                                     ros::Duration(0.1),
                                      &errMsg);
 
       pcl_ros::transformPointCloud(_processingFrame, *cloud, *cloudInProcFrame, _tfListener);
